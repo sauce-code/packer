4c8956f3f50cfcfbc02e71a1eb422351781546c9
Torben <mail@sauce-code.com>
2017-08-24 22:41:37 +0200

    add equals and hashcode

--- src/main/java/com/saucecode/packer/Item.java
+++ src/main/java/com/saucecode/packer/Item.java
@@ -97,4 +97,41 @@ public class Item {
 		return "Item [name=" + name + ", category=" + category + ", sets=" + sets + "]";
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sets == null) {
+			if (other.sets != null)
+				return false;
+		} else if (!sets.equals(other.sets))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sets == null) ? 0 : sets.hashCode());
+		return result;
+	}
+
 }
