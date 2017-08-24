b39cad76a9e58336fe1fd61697df88b4baa803b6
Torben <mail@sauce-code.com>
2017-08-24 22:41:34 +0200

    add equals and hashcode

--- src/main/java/com/saucecode/packer/ItemContainer.java
+++ src/main/java/com/saucecode/packer/ItemContainer.java
@@ -50,4 +50,29 @@ public class ItemContainer {
 		return "Items [item=" + items + "]";
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemContainer other = (ItemContainer) obj;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		return result;
+	}
+
 }
