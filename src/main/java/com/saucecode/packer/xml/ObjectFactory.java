26e6f8875af4095a4160f57d10a631fd5ab94341
Torben <mail@sauce-code.com>
2017-09-08 15:20:15 +0200

    add string types hierarchy and estrictions

--- src/main/java/com/saucecode/packer/xml/ObjectFactory.java
+++ src/main/java/com/saucecode/packer/xml/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2017.09.03 um 10:24:15 PM CEST 
+// Generiert: 2017.09.08 um 03:06:37 PM CEST 
 //
 
 
@@ -34,6 +34,7 @@ public class ObjectFactory {
     private final static QName _Set_QNAME = new QName("http://www.saucecode.com/packer/xml", "set");
     private final static QName _Category_QNAME = new QName("http://www.saucecode.com/packer/xml", "category");
     private final static QName _Name_QNAME = new QName("http://www.saucecode.com/packer/xml", "name");
+    private final static QName _Description_QNAME = new QName("http://www.saucecode.com/packer/xml", "description");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.saucecode.packer.xml
@@ -107,6 +108,15 @@ public class ObjectFactory {
     @XmlElementDecl(namespace = "http://www.saucecode.com/packer/xml", name = "name")
     public JAXBElement<String> createName(String value) {
         return new JAXBElement<String>(_Name_QNAME, String.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://www.saucecode.com/packer/xml", name = "description")
+    public JAXBElement<String> createDescription(String value) {
+        return new JAXBElement<String>(_Description_QNAME, String.class, null, value);
     }
 
 }
