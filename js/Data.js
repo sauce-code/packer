073b4fcbc70a2adcfcd56d03c540815145d7efe5
Torben <sauce-code@users.noreply.github.com>
2023-12-24 12:42:30 +0100

    Create Data.js

---
+++ js/Data.js
@@ -0,0 +1,41 @@
+export default class Data {
+
+    static CATEGORIES = [
+        {
+            id: "#allgemein",
+            name: "Allgemein",
+            description: "Allgemeine Gegenstände, die man auf jeder Packliste haben möchte."
+        },
+        {
+            id: "#kleidung",
+            name: "Kleidung",
+            description: "Kleidung zum Anziehen. Sollte niemals fehlen."
+        }
+    ]
+
+    static CONDITIONS = [
+        {
+            id: "#winter",
+            name: "Winter",
+            description: "Diese eine kalte Jahreszeit. Erforert entsprechende Anpassungen beim Packen."
+        }
+    ]
+
+    static ITEMS = [
+        {
+            id: "#handy",
+            name: "Handy + Ladekabel",
+            category: "#allgemein",
+            conditions: [],
+            description: "Der Grundstein jeglicher Kommunikation. Ohne Ladekabel schnell wertlos."
+        },
+        {
+            id: "#muetze",
+            name: "Mütze",
+            category: "#kleidung",
+            conditions: ["#winter"],
+            description: "Kommt auf den Kopf. Hät warm. Ziemlich geil."
+        }
+    ]
+
+}
